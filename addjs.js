7fc35ba5f0d4cf8dfedd5c43c9b9ffa1d6212c4e
SystemOfProject42 <150487038+SystemOfProject42@users.noreply.github.com>
2023-11-28 05:51:43 +0000

    + home button and <p> design base finalized ~ found some bugs ~ still need to correct small stuff (hommebuttonglitch), also see if we rly need all da wrapper + buttons javscript function for animations and styles implemented ? maybe set timeout function setIntervall for animation back again?

--- addjs.js
+++ addjs.js
@@ -1,33 +1,38 @@
 //function visionplay() {
 //  document.getElementsByClassName(".wrapper-holderhome").style.animationPlayState = "running";
 //}
-const wrapperhome = document.querySelector("wrapperhomeclick");
 const wrapper = document.querySelector(".wrapper-holderhome");
 
 const buttonmission = document.querySelector(".buttonm");
 const buttonvision = document.querySelector(".buttonv");
 
+const panimatedv = document.querySelector(".panimatedv");
+const panimatedm = document.querySelector(".panimatedm");
 
 buttonmission.addEventListener("click", function () {
 
-  wrapper.classList.remove("homeanimation");
-  wrapper.style.transform = "translateX(-100%)";
+  panimatedm.classList.remove("homeanimationv");
+  panimatedm.classList.remove("homeanimationm");
 
   buttonmission.classList.remove("missionanimation");
   buttonvision.classList.remove("visionanimation");
 
+  panimatedv.style.opacity = "0";
+  panimatedm.style.opacity = "1";
   buttonmission.style.background = "radial-gradient(#23b499dd 10%,#2362b4dd)";
   buttonvision.style.background = "radial-gradient(#23a7a766 10%,#2362b444)";
 });
 
 buttonvision.addEventListener("click", function () {
 
-  wrapper.classList.remove("homeanimation");
-  wrapper.style.transform = "translateX(0%)";m
+  panimatedm.classList.remove("homeanimationm");
+  panimatedv.classList.remove("homeanimationv");
 
   buttonvision.classList.remove("visionanimation");
   buttonmission.classList.remove("missionanimation");
 
+  panimatedv.style.opacity = "1";
+  panimatedm.style.opacity = "0";
   buttonvision.style.background = "radial-gradient(#23b499dd 10%,#2362b4dd)";
   buttonmission.style.background = "radial-gradient(#23a7a766 10%,#2362b444)";
 });
